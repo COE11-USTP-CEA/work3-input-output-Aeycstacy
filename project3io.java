4e7503ec220c5c2cfc50e400b1b52ab2cb413ea9
Aeycstacy <aey.toroba@gmail.com>
2017-01-27 23:04:07 +0800

    3rd Work

---
+++ project3io.java
@@ -0,0 +1,10 @@
+class project3io{
+	public static void main(String[]args){
+		for(String j: args){
+			System.out.println("Hello " + j );
+		}
+		System.out.println("There are " + args.length + "Creatures");
+		System.out.println("Hello Universe!");
+		System.out.println("I'm Angel Sagrado :)");
+    }
+}
